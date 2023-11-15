371ed15c5ac18bbf51827c6b069ce9a0b503607c
Okan ÇEÇEN <okancecen@Okan-MacBook-Air.local>
2023-11-15 16:42:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './main.css';
 
-function App() {
+const Timer = () => {
+  const initialMinutes = 2;
+  const [minutes, setMinutes] = useState(initialMinutes);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        if (seconds === 0) {
+          if (minutes === 0) {
+            clearInterval(interval);
+            setIsActive(false);
+            alert('2 dakika doldu!');
+            setMinutes(initialMinutes);
+            setSeconds(0);
+          } else {
+            setMinutes((prev) => prev - 1);
+            setSeconds(59);
+          }
+        } else {
+          setSeconds((prev) => prev - 1);
+        }
+      }, 1000);
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="timer">
+      <div className="display">
+        <span>{String(minutes).padStart(2, '0')}:</span>
+        <span>{String(seconds).padStart(2, '0')}</span>
+      </div>
+      <div className="controls">
+        <button>Başlat</button>
+        <button >Duraklat</button>
+        <button >Sıfırla</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Timer;
